83854286449e176295ae831cb5f23283cb0e0ee9
Andrew Chan <andrew.chan@bigcommerce.com>
2022-03-04 13:15:39 +1100

    test2

--- src/Domain/ShippingRates/ShippingRateAPIController.php
+++ src/Domain/ShippingRates/ShippingRateAPIController.php
@@ -2,6 +2,8 @@
 
 namespace BCSample\Shipping\Domain\ShippingRates;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\TransferStats;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,31 +24,54 @@ class ShippingRateAPIController
 
     public function getRates(Request $request)
     {
+        error_log('is this updated');
+
         $requestPayload = json_decode($request->getContent(), true);
 
         if (!$this->validatePayload($requestPayload)) {
             return new JsonResponse($this->buildErrorResponseBody('Badly formatted request'));
         }
 
-        $requestData =  json_decode($request->getContent(), true);
-        $storeHash = $requestData['base_options']['store_id'];
-        $cartId = $requestData['base_options']['request_context']['reference_values'][0]['value'];
+        $storeHash = $requestPayload['base_options']['store_id'];
+        $cartId = $requestPayload['base_options']['request_context']['reference_values'][0]['value'];
 
         error_log(':::cURL request to Checkout API:::');
+        error_log("::cart_id::$cartId");
         $startTime = microtime(true);
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
-        curl_setopt($ch, CURLOPT_URL, "https://api.service.bcdev/stores/{$storeHash}/v3/checkouts/{$cartId}");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'X-Auth-Token: o80wd9luhsbh2i50kysuxpuxkvtlhm',
-            'X-Auth-Client: 36ko3nsufy5xm7f33ij6pdaeqs2ti33',
-            'Accept: application/json',
-            'Content-Type: application/json',
-        ]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        curl_close($ch);
+        $client = $this->setupHttpClient(
+            [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+                'X-Auth-Token' => $requestPayload['connection_options']['x_auth_token'],
+                'X-Auth-Client' => $requestPayload['connection_options']['x_auth_client'],
+            ],
+            null,
+            false
+        );
+
+        $checkoutResponse = $client->request(
+            'GET',
+            "https://api.service.bcdev/stores/{$storeHash}/v3/checkouts/{$cartId}"
+        );
+
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
+//        curl_setopt($ch, CURLOPT_URL, "https://store-{$storeHash}.store.bcdev/internalapi/v2/checkouts/{$cartId}?XDEBUG_SESSION_START=1");
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+//            'Accept: application/json',
+//            'Content-Type: application/json',
+//        ]);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        $output = curl_exec($ch);
+//        $result = curl_getinfo($ch);
+//        error_log("::checkout_output_call::$output");
+//        curl_close($ch);
+
+        $checkoutOutputData = $checkoutResponse->getBody();
+
+        error_log("::checkout_output_call::$checkoutOutputData");
+
         $endTime = microtime(true);
 
         error_log(':::cURL Time:::' . ($endTime - $startTime));
@@ -75,5 +100,27 @@ class ShippingRateAPIController
     private function validatePayload($requestPayload)
     {
         return !is_null($requestPayload) && is_array($requestPayload);
+    }
+
+    /**
+     * Initialises underlying Http client
+     *
+     * @param array $headers
+     * @param array|null $basicAuth
+     * @param bool|true $verifySSLCert
+     * @param int|null $timeout
+     */
+    public function setupHttpClient($headers = [], $basicAuth = null, $verifySSLCert = true, $timeout = null)
+    {
+        $config = [
+            'timeout' => $timeout ?: 60,
+            'verify' => $verifySSLCert,
+            'headers' => $headers,
+            'http_errors' => true
+        ];
+        if ($basicAuth) {
+            $config['auth'] = $basicAuth;
+        }
+        return new Client($config);
     }
 }
